afd9663b6aa9ea9d1029e56264ca673bbc936fe1
arunram.krish@gmail.com <arunram.krish@gmail.com>
2015-07-17 10:55:54 +0530

    labs with clock example  and local storage

--- labs/form.js
+++ labs/form.js
@@ -25,8 +25,42 @@ function LocationStore() {
 	}
 	this.printLocations = function() {
 		for(var l in locations) {
-			console.log(l.toString());
+			console.log(locations[l].toString());
 		}
 	}
 }
-window.locationStore = new LocationStore();
+//window.locationStore = new LocationStore();
+window.locationStore = new LocalStore();
+
+function LocalStore() {
+
+	this.addLocation = function (loc) {
+		var locations;
+		if (localStorage.getItem('locations')) {
+			locations = JSON.parse(localStorage.getItem('locations'));
+		} else {
+			locations = new Array();
+		}
+		locations.push(loc);
+		var locationsAsString = JSON.stringify(locations);
+		localStorage.setItem('locations', locationsAsString);
+	}
+	this.printLocations = function() {
+		var output = "";
+		if (window.localStorage) {
+    		if ((localStorage.length) && (localStorage.getItem('locations'))) {
+    			var locations = JSON.parse(localStorage.getItem('locations'));
+    			for(var l in locations) {
+					output += "\n" + locations[l].location + ":" + locations[l].tag;
+				}
+
+    		} else {
+       			output += 'There is no data stored for this domain.';
+    		}
+		} else {
+    		output += 'Your browser does not support local storage.'
+		}
+		console.log(output);
+	}
+
+}
